60429bf7dbe858b6c74534ff8dc67777f479fa5e
maxiluckies <luckiesmaxi6@gmail.com>
2019-08-01 22:16:39 +0700

    change reward mechanism

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -13,42 +13,52 @@ class TransactionController extends Controller
 {
     public function store(Request $request, $id)
     {
-			$user = User::findOrFail(Auth::user()->username);
-			$product = Product::findOrFail($id);
-            $trans = new Transaction;
-			$trans->product = $id;
-            $trans->customer = Auth::user()->username;
-			$trans->ammount = $request->ammount;
-			$product->stock = $product->stock - $trans->ammount;
-			$total = $trans->ammount * $product->price;
-			$rewardAdded = $this->addReward($user,$total);
-			$trans->save();
-			$product->save();
-            return array('success'=>array('product'=>$product->name,'ammount'=>$trans->ammount,'total'=>$total,'reward'=>$rewardAdded));
+        $user = User::findOrFail(Auth::user()->username);
+        $product = Product::findOrFail($id);
+        $trans = new Transaction;
+        $trans->product = $id;
+        $trans->customer = Auth::user()->username;
+        $trans->ammount = $request->ammount;
+        $product->stock = $product->stock - $trans->ammount;
+        $total = $trans->ammount * $product->price;
+        $user->point = $user->point + 1;
+        $user->save();
+        $trans->save();
+        $product->save();
+        return array('success' => array('product' => $product->name, 'ammount' => $trans->ammount, 'total' => $total));
 
 
     }
 
-	public function buyerList(Request $request){
+    public function buyerList(Request $request)
+    {
         $trans = Transaction::whereHas('product', function ($query) {
             $query->where('user_id', Auth::user()->username);
         })->get();
 
         return $trans;
-	}
+    }
 
-	protected function addReward(User $user,$total){
-        $reward = 0;
-		if($total > 1000){
-			$user->reward = $user->reward + 40;
-			$user->save();
-			$reward = 40;
-		} else if($total > 500){
-			$user->reward = $user->reward + 20;
-			$user->save();
-			$reward = 20;
-		}
-		return $reward;
-	}
+    protected function getReward($id)
+    {
+        $user = User::findOrFail(Auth::user()->username);
 
+        //Reward A
+        if ($id == 1) {
+            return $this->tradePoint($user,20,'Reward A');
+        } else if ($id == 2){
+            return $this->tradePoint($user,40,'Reward B');
+        }
+    }
+
+    protected function tradePoint(User $user , int $pointNeeded, string $rewardName){
+        $userPoint = $user->point;
+        if ($userPoint >= $pointNeeded) {
+            $user->point = $userPoint - $pointNeeded;
+            $user->save();
+            return $rewardName.' acquired';
+        } else {
+            return "Your point is not enough, '.$rewardName.' need minimum of '.$pointNeeded.' point";
+        }
+    }
 }
